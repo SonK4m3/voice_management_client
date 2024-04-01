80d8561aa98b8402cfed9b2ee36c893f208be8a9
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-04-01 16:53:19 +0700

    chore: make color role

--- src/pages/navbar/Profile.jsx
+++ src/pages/navbar/Profile.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import OverFlowGraphy from "../../components/OverFlowTypography";
+import Stack from "@mui/material/Stack";
 
 const Profile = () => {
-  const { name } = useSelector((state) => state.auth.user);
+  const { name, role } = useSelector((state) => state.auth.user);
 
   return (
     <Box>
-      <OverFlowGraphy variant="h4" component="h4">
-        {name || "User001"}
-      </OverFlowGraphy>
+      <Stack spacing={2}>
+        <OverFlowGraphy variant="h4" component="h4">
+          {name || "User001"}
+        </OverFlowGraphy>
+        <OverFlowGraphy
+          variant="caption"
+          color={role === "user" ? "beige" : "green"}
+        >
+          {role || ""}
+        </OverFlowGraphy>
+      </Stack>
     </Box>
   );
 };
